1724489a75c81c471fd9ffe31bf1763e17cb4b4d
nhatdaithienvuong <130645903+nhatdaithienvuong@users.noreply.github.com>
2024-02-25 09:43:08 +0700

    update catch

--- infinite-scrolling/src/utils/post/getPost.ts
+++ infinite-scrolling/src/utils/post/getPost.ts
@@ -2,4 +2,5 @@
 export const getPost = ({ id }: { id?: string }) => {
     return fetch(`https://dummyjson.com/products/${id}`)
         .then(res => res.json())
+        .catch((err) => err)
 }
--- infinite-scrolling/src/utils/post/getPosts.ts
+++ infinite-scrolling/src/utils/post/getPosts.ts
@@ -1,5 +1,6 @@
 
-export const getPosts = ({ query, limit }: { query: string, limit: number}) => {
+export const getPosts = ({ query, limit }: { query: string, limit: number }) => {
     return fetch(`https://dummyjson.com/products/search?q=${query || ''}&limit=${limit}`)
         .then(res => res.json())
+        .catch((err) => err)
 }
